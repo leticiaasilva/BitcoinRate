d9db3e29563591313d9fca6fa7bb6ff700dd0105
leticiaasilva <69753760+leticiaasilva@users.noreply.github.com>
2021-08-25 16:17:17 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft;
+
+namespace bitcoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json;
+
+            using (var web = new System.Net.WebClient())
+            {
+            var url = @"https://api.coindesk.com/v1/bpi/currentprice.json";
+            json = web.DownloadString(url);
+            }
+
+            dynamic obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            var currentPrice = Convert.ToDecimal(obj.bpi.EUR.rate.Value);
+
+            Console.WriteLine(currentPrice);
+        }
+
+        
+    }
+}
